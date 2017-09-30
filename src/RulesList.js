d24e83715793990c1cd797af8fafa70766ee6422
André Herculano <andresilveirah@gmail.com>
2017-09-30 17:12:14 +0200

    add onRemoveClick prop to RulesList

--- src/RulesList.js
+++ src/RulesList.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const RulesList = ({ rules }) => (
+const RulesList = ({ rules, onRemoveClick }) => (
   <div>
     <h2>2. Rules list</h2>
     <ul>
-      { rules.map(({id, title}) => <li key={`rule-${id}`}>{title}</li>) }
+      { rules.map(({id, title}) =>
+        <li key={`rule-${id}`}>
+          {title}<button id={`remove-rule-${id}`} onClick={() => onRemoveClick(id)}>&times;</button>
+        </li>)
+      }
     </ul>
   </div>
 );
--- src/RulesList.spec.js
+++ src/RulesList.spec.js
@@ -4,10 +4,30 @@ import { shallow } from 'enzyme';
 import RulesList from './RulesList';
 
 describe('RulesList', () => {
+  let rules, rulesList, onRemoveClickMock;
+
+  beforeEach(() => {
+    rules = [{ title: 'a', id: '1' }, { title: 'b', id: '2'}];
+    onRemoveClickMock = jest.fn();
+    rulesList = shallow(<RulesList rules={rules} onRemoveClick={onRemoveClickMock} />);
+  });
+
   it('renders a li element for each rule in its rules prop', () => {
-    const rules = [{ title: 'a', id: '1' }, { title: 'b', id: '2'}];
-    const rulesList = shallow(<RulesList rules={rules} />);
-    expect(rulesList).toContainReact(<li>a</li>);
-    expect(rulesList).toContainReact(<li>b</li>);
+    expect(rulesList.containsMatchingElement(
+      <li>a<button>&times;</button></li>
+    )).toBeTruthy();
+    expect(rulesList.containsMatchingElement(
+      <li>b<button>&times;</button></li>
+    )).toBeTruthy();
+  });
+
+  describe('when clicking on the remove link', () => {
+    beforeEach(() => {
+      rulesList.find('#remove-rule-1').simulate('click');
+    });
+
+    it('calls the onRemoveClick prop passing the rules id as its parameter', () => {
+      expect(onRemoveClickMock).toHaveBeenCalledWith("1");
+    });
   });
 });
