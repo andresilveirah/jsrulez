fb3ea9453e3b8bb05b6edbf0193a68369fc1c447
André Herculano <andresilveirah@gmail.com>
2017-10-11 20:40:45 +0200

    display error when executing broken flow

--- src/FlowExecutor.spec.js
+++ src/FlowExecutor.spec.js
@@ -4,10 +4,16 @@ import { shallow } from 'enzyme';
 import FlowExecutor from './FlowExecutor';
 import ResultsList from './ResultsList';
 import ExecuteFlowForm from './ExecuteFlowForm';
+import ValidatorMessage from './ValidatorMessage';
 
 function* engineMock(testingObject) {
   yield({ currentRule: `first-rule-${testingObject}`, passed: true });
   yield({ currentRule: `second-rule-${testingObject}`, passed: false });
+}
+
+function* brokenEngine(testingObject) {
+  yield 'it is a trap';
+  throw Error('kaputt');
 }
 
 describe('FlowExecutor', () => {
@@ -26,6 +32,13 @@ describe('FlowExecutor', () => {
     expect(flowExecutor).toContainReact(<ResultsList results={[]} />);
   });
 
+  describe('when the error state is not empty', () => {
+    it('renders a ValidatorMessage passing the error state', () => {
+      flowExecutor.setState({ error: 'kaputt'});
+      expect(flowExecutor).toContainReact(<ValidatorMessage message='kaputt' />);
+    });
+  });
+
   describe('onExecuteFlow', () => {
     beforeEach(() => { onExecuteFlow('testing'); });
 
@@ -34,6 +47,17 @@ describe('FlowExecutor', () => {
         { rule: 'first-rule-testing', passed: true },
         { rule: 'second-rule-testing', passed: false }
       ])
+    });
+
+    describe('if the engine throws an error', () => {
+      beforeEach(() => {
+        flowExecutor = shallow(<FlowExecutor engine={brokenEngine} />);
+        flowExecutor.instance().onExecuteFlow();
+      });
+
+      it('sets the error state', () => {
+        expect(flowExecutor).toHaveState('error', 'kaputt');
+      });
     });
   });
 });
